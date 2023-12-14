39b6cf7f389e5868e8ff816b9c3bf9592264442d
Drischdaan <lpboy100@gmail.com>
2023-12-15 00:14:08 +0100

    feat: adds crash dump

--- Engine/Source/Runtime/Engine/Private/Engine/Platform/Launch.cpp
+++ Engine/Source/Runtime/Engine/Private/Engine/Platform/Launch.cpp
@@ -4,8 +4,34 @@
 
 #include "Engine/Engine.h"
 
+#include <windows.h>
+#include <Dbghelp.h>
+
+typedef BOOL (WINAPI *MINIDUMPWRITEDUMP)(HANDLE hProcess, DWORD dwPid, HANDLE hFile, MINIDUMP_TYPE DumpType,
+                                         PMINIDUMP_EXCEPTION_INFORMATION ExceptionParam,
+                                         PMINIDUMP_USER_STREAM_INFORMATION UserStreamParam,
+                                         PMINIDUMP_CALLBACK_INFORMATION CallbackParam);
+
+LONG WINAPI HandleException(_EXCEPTION_POINTERS* apExceptionInfo)
+{
+	const HMODULE DebugHelperModuleHandle = LoadLibrary(TEXT("dbghelp.dll"));
+	const MINIDUMPWRITEDUMP WriteDumpFunction = reinterpret_cast<MINIDUMPWRITEDUMP>(GetProcAddress(
+		DebugHelperModuleHandle, "MiniDumpWriteDump"));
+	const HANDLE FileHandle = CreateFile(TEXT("core.dmp"), GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, CREATE_ALWAYS,
+	                                     FILE_ATTRIBUTE_NORMAL, nullptr);
+	_MINIDUMP_EXCEPTION_INFORMATION ExInfo;
+	ExInfo.ThreadId = GetCurrentThreadId();
+	ExInfo.ExceptionPointers = apExceptionInfo;
+	ExInfo.ClientPointers = FALSE;
+	WriteDumpFunction(GetCurrentProcess(), GetCurrentProcessId(), FileHandle, MiniDumpNormal, &ExInfo, nullptr,
+	                  nullptr);
+	CloseHandle(FileHandle);
+	return EXCEPTION_CONTINUE_SEARCH;
+}
+
 uint32_t LaunchSnowbite(int argc, char* argv[])
 {
+	SetUnhandledExceptionFilter(HandleException);
 	FEngine::EngineInstance = std::make_shared<FEngine>();
 	const HRESULT initializeResult = GetEngine()->Initialize();
 	if (SUCCEEDED(initializeResult))
